c16dca505d8502e05458bb898531ea5b21fbe971
rharbi <iirharbi@gmail.com>
2026-01-27 01:02:15 +0300

    Create Restaurant entity

--- src/main/java/com/rayan/restaurant/domain/OperatingHours.java
+++ src/main/java/com/rayan/restaurant/domain/OperatingHours.java
@@ -11,7 +11,7 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class operatingHours {
+public class OperatingHours {
 
     @Field(type = FieldType.Nested)
     private TimeRange monday;
