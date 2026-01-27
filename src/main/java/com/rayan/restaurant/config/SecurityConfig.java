795ecdf404b170dea22942321921908123da2edb
rharbi <iirharbi@gmail.com>
2026-01-27 22:10:47 +0300

    Create SecurityConfig

---
+++ src/main/java/com/rayan/restaurant/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.rayan.restaurant.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
